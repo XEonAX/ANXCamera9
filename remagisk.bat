c500376f54a7f48efacc81a8f96170e413f51337
XEonAX <sv.xeon@gmail.com>
2019-01-04 23:32:42 +0530

    Version Name in Zip

--- remagisk.bat
+++ remagisk.bat
@@ -1,5 +1,8 @@
-
+for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
+echo %anxversion%
 xcopy /s /y .\out\ANXCamera.apk .\src\ANXCameraMagisk\system\priv-app\ANXCamera
 del .\out\ANXCameraMagisk.zip
+del .\out\ANXCameraMagisk_*.zip
 "C:\Program Files\7-Zip\7z.exe" a -tzip .\out\ANXCameraMagisk.zip .\src\ANXCameraMagisk\*
+copy .\out\ANXCameraMagisk.zip /B .\out\ANXCameraMagisk_%anxversion%.zip
 adb push .\out\ANXCameraMagisk.zip  /sdcard/zips
