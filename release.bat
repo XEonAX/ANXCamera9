20a7feeabfdaf7289c76fc40f805c71c55b8b09a
XEonAX <sv.xeon@gmail.com>
2019-01-19 15:45:08 +0530

    Update redo.bat and release.bat

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
 echo %anxversion%
-rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m polaris.BleedingEdge BleedingEdge.polaris
-..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m polaris.%anxversion% %anxversioncode%.polaris
-rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode%
+rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m polaris.BleedingEdge BleedingEdge.polaris -t polaris
+..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m polaris.%anxversion% %anxversioncode%.polaris -t polaris
+rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode% -t polaris
