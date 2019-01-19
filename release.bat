3f0ae139c88288778e3ab57a49b6da72b05bfc60
XEonAX <sv.xeon@gmail.com>
2019-01-19 14:18:33 +0530

    Update release script

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
 echo %anxversion%
-..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m BleedingEdge BleedingEdge
-REM..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m %anxversion% %anxversioncode%
-..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode%
+rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m beryllium.BleedingEdge BleedingEdge.beryllium
+..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m beryllium.%anxversion% %anxversioncode%.beryllium
+rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode%
