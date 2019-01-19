3277a6fe6abaa295ce1a9ef643edce2c706b6d2d
XEonAX <sv.xeon@gmail.com>
2019-01-19 14:25:55 +0530

    Update release script
    (cherry picked from commit 3f0ae139c88288778e3ab57a49b6da72b05bfc60)
    
    # Conflicts:
    #	release.bat
    (cherry picked from commit ae921013bc2258fdd3bcdece727a48fa03f49000)

---
+++ release.bat
@@ -0,0 +1,5 @@
+for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
+echo %anxversion%
+rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m polaris.BleedingEdge BleedingEdge.polaris
+..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m polaris.%anxversion% %anxversioncode%.polaris
+rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode%
