c71a6500cb3e08db70e3cd138df6e0a055fec405
XEonAX <sv.xeon@gmail.com>
2019-01-19 14:29:09 +0530

    Update redo.bat and release.bat

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
 echo %anxversion%
-rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m dipper.BleedingEdge BleedingEdge.dipper
-..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m dipper.%anxversion% %anxversioncode%.dipper
-rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode%
+rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m dipper.BleedingEdge BleedingEdge.dipper -t dipper
+..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m dipper.%anxversion% %anxversioncode%.dipper -t dipper
+rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode% -t dipper
