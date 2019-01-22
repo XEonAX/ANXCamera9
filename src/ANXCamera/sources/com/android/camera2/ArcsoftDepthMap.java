2eb1bca63ba4b31f166f0ee5b071376c06d68418
XEonAX <sv.xeon@gmail.com>
2019-01-22 22:32:00 +0530

    Fix build.prop

--- src/ANXCamera/sources/com/android/camera2/ArcsoftDepthMap.java
+++ src/ANXCamera/sources/com/android/camera2/ArcsoftDepthMap.java
@@ -78,7 +78,7 @@ public class ArcsoftDepthMap {
     }
 
     /* JADX WARNING: Removed duplicated region for block: B:109:0x0274 A:{ExcHandler: java.io.IOException (e java.io.IOException), Splitter: B:69:0x0227} */
-    /* JADX WARNING: Removed duplicated region for block: B:108:0x0272 A:{ExcHandler: java.io.IOException (e java.io.IOException), PHI: r10 , Splitter: B:78:0x0248} */
+    /* JADX WARNING: Removed duplicated region for block: B:108:0x0272 A:{PHI: r10 , ExcHandler: java.io.IOException (e java.io.IOException), Splitter: B:78:0x0248} */
     /* JADX WARNING: Missing block: B:110:0x0275, code:
             r10 = null;
      */
