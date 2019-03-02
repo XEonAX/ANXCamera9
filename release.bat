e6338a611cc3bfbe90ec6230c91e0bea7f6772aa
XEonAX <sv.xeon@gmail.com>
2019-03-03 00:12:32 +0530

    oos release script update

--- release.bat
+++ release.bat
@@ -1,9 +1,9 @@
 for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
 echo %anxversion%
-git tag %anxversioncode%.singularity
-git push origin %anxversioncode%.singularity
-rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m singularity.BleedingEdge BleedingEdge.singularity
+git tag %anxversioncode%.oosberyllium
+git push origin %anxversioncode%.oosberyllium
+rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m oosberyllium.BleedingEdge BleedingEdge.oosberyllium
 rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode%
-..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m singularity.%anxversion% %anxversioncode%.singularity
-..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraUnity_%anxversion%.zip -m singularity.%anxversion% %anxversioncode%.singularity
-rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANX4K60Unity_%anxversion%.zip -m singularity.%anxversion% %anxversioncode%.singularity
+..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m oosberyllium.%anxversion% %anxversioncode%.oosberyllium
+..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraUnity_%anxversion%.zip -m oosberyllium.%anxversion% %anxversioncode%.oosberyllium
+rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANX4K60Unity_%anxversion%.zip -m oosberyllium.%anxversion% %anxversioncode%.oosberyllium
