24b77678333d111e82a02f5690e6641cbca869aa
XEonAX <sv.xeon@gmail.com>
2019-03-10 20:13:14 +0530

    default watermarks

--- release.bat
+++ release.bat
@@ -6,4 +6,4 @@ rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m sin
 rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode%
 ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk_%anxversion%.zip -m singularity.%anxversion% %anxversioncode%.singularity
 ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraUnity_%anxversion%.zip -m singularity.%anxversion% %anxversioncode%.singularity
-..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANX4K60Unity_%anxversion%.zip -m singularity.%anxversion% %anxversioncode%.singularity
+rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANX4K60Unity_%anxversion%.zip -m singularity.%anxversion% %anxversioncode%.singularity
